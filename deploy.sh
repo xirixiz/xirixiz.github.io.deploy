c0c8972555e49798dbcaa0ba3932406538cdb9e3
Bram van Dartel <bram.vandartel@kvk.nl>
2017-06-14 10:26:55 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 set -x
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-# setup constants
+
 REPO="xirixiz.github.io"
 GH_REPO="github.com/xirixiz/${REPO}.git"
 MSG=$(git log -1 --oneline)
+
 git config --global user.email "spam@rootrulez.com"
 git config --global user.name "Travis CI"
 
-# Build the project.
-hugo -t hugo-sustain
+echo "Cloning target repository"
+git clone "https://$GH_REPO" public
+
+echo "Running hugo"
+hugo
 ls -al public
 
-git clone "https://$GH_REPO"
-cd ${REPO} && mv CNAME ../public && rm -rf *
-ls -al ../public
-cd .. && cp -r public/* ${REPO}
-ls -al ${REPO}
-cd ${REPO}
+echo "Publishing to branch"
+cd public
+git add -A .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://$GH_REPO" master:master
 
-#git remote
-#git add -A :/
-git add -A
-git commit -a -m "via travis -- for $MSG"
-git push "https://${GH_TOKEN}@${GH_REPO}" master > /dev/null 2>&1
-
-#git clone "${GIT_DEPLOY_REPO}"
-#git push "${GIT_DEPLOY_REPO}" master > /dev/null 2>&1
+echo "Done"
