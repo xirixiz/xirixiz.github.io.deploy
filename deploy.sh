314aa195fee216cf4469a17092629c34830493bd
Bram van Dartel <bram.vandartel@kvk.nl>
2017-06-14 10:36:12 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ git config --global user.email "spam@rootrulez.com"
 git config --global user.name "Travis CI"
 
 echo "Cloning target repository"
+rm -rf public
 git clone "https://$GH_REPO" public
 
 echo "Running hugo"
@@ -16,7 +17,7 @@ hugo
 
 echo "Publishing to branch"
 cd public
-git add -A .
+git add .
 git commit -m "Deployed to Github Pages - ${MSG}"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REPO}" master:master
 
